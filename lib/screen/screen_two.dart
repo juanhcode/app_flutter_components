05089274ad6e99fcea39209fc2a6667d97fd3d01
Juan Hoyos <juanhoyos1347@gmail.com>
2022-06-06 20:14:11 -0500

    segunda screen

---
+++ lib/screen/screen_two.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ScreenTwo extends StatelessWidget {
+  const ScreenTwo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Pagina 2')
+    );
+  }
+}

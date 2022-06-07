5838b4e70a78254608331a6ada6c922a94d00274
Juan Hoyos <juanhoyos1347@gmail.com>
2022-06-06 20:13:53 -0500

    priemra screen

---
+++ lib/screen/screen_one.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScreenOne extends StatelessWidget {
+  const ScreenOne({Key? key}) : super(key: key);
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Pagina una'),
+      ),
+    );
+  }
+}

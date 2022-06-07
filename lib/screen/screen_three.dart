3b653174395910e115370b83ca56d0177447eaeb
Juan Hoyos <juanhoyos1347@gmail.com>
2022-06-06 21:38:48 -0500

    animacion creada en la pantalla

--- lib/screen/screen_three.dart
+++ lib/screen/screen_three.dart
@@ -1,12 +1,57 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class ScreenThree extends StatelessWidget {
+class ScreenThree extends StatefulWidget {
   const ScreenThree({Key? key}) : super(key: key);
 
   @override
+  State<ScreenThree> createState() => _ScreenThreeState();
+}
+
+class _ScreenThreeState extends State<ScreenThree> {
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.red;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(10);
+
+  void changeShape() {
+    final random = Random();
+    _width = random.nextInt(300).toDouble() + 70;
+    _height = random.nextInt(300).toDouble() + 70;
+    _color = Color.fromRGBO(
+        random.nextInt(255), random.nextInt(255), random.nextInt(255), 1);
+    _borderRadius = BorderRadius.circular(random.nextInt(100).toDouble() + 70);
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Pagina tres'),
-    );
+    return Center(
+        child: Column(
+      children: [
+        const SizedBox(
+          height: 70,
+        ),
+        AnimatedContainer(
+          width: _width,
+          height: _height,
+          decoration: BoxDecoration(
+            color: _color,
+            borderRadius: _borderRadius,
+          ),
+          duration: const Duration(milliseconds: 400),
+          curve: Curves.easeInOutCubic,
+        ),
+        const SizedBox(
+          height: 50,
+        ),
+        FloatingActionButton(
+            onPressed: () {
+              changeShape();
+            },
+            child: const Icon(Icons.play_arrow_outlined))
+      ],
+    ));
   }
 }

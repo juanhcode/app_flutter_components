b1a217db18e30d511039962c4e81caa3c92c5d82
Juan Hoyos <juanhoyos1347@gmail.com>
2022-06-06 20:12:56 -0500

    se agrega la funcionalidad de rutas inamicas

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -2,6 +2,18 @@ import 'package:app_flutter_components/screen/screen.dart';
 import 'package:flutter/material.dart';
 
 class Routes {
+  final int index;
+  const Routes({required this.index});
+
+  Widget miPages() {
+    List<Widget> pages = [
+      const ScreenOne(),
+      const ScreenTwo(),
+      const ScreenThree(),
+    ];
+    return pages[index];
+  }
+
   static String rutaInicial = '/home';
 
   static Map<String, Widget Function(BuildContext)> appRoutes() {
